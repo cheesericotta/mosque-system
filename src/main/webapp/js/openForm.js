777bdf7b64f9babd190c937bf550658ce54a0eb6
cheesericotta <79610712+cheesericotta@users.noreply.github.com>
2022-07-17 21:38:54 +0800

    Delete src/main directory

--- src/main/webapp/js/openForm.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-    document.getElementById("logout-form").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("logout-form").style.display = "none";
-  }
